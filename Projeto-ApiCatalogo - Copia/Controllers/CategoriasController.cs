b4bc4f5eaa50ee5c863e7e3a601544e188a7710c
GabrielGrozz <gabrielgrozz.net@gmail.com>
2024-02-19 14:15:22 -0300

    aiojjsd

---
+++ Projeto-ApiCatalogo - Copia/Controllers/CategoriasController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projeto_ApiCatalogo.Context;
+using Projeto_ApiCatalogo.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Projeto_ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("products")]
+        public ActionResult<IEnumerable<Category>> GetCategoryAndProducts()
+        {
+            return _context.Categories.AsNoTracking().Include(e => e.Products).ToList();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> Get()
+        {
+
+            return _context.Categories.AsNoTracking().ToList();
+                        
+        }
+
+        [HttpGet("{id:int}", Name ="GetCategory")]
+        public ActionResult<Category> Get(int id) 
+        {
+            Category categorie = _context.Categories.AsNoTracking().FirstOrDefault(e => e.CategoryId == id);
+
+            if(categorie == null)
+            {
+                return NotFound();
+            }
+
+            return categorie;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Category categorie)
+        {
+            if(categorie == null)
+            {
+                return BadRequest();
+
+            }
+            _context.Categories.Add(categorie);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("GetCategory", new { id = categorie.CategoryId }, categorie);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id ,Category categorie) 
+        {
+            if(id != categorie.CategoryId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categorie).State = EntityState.Modified;
+            _context.SaveChanges(true);
+
+            return Ok(categorie);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            Product product = _context.Products.FirstOrDefault(e => e.ProductId == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
+    }
+}
---
+++ Projeto-ApiCatalogo - Copia/Controllers/ProductsController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projeto_ApiCatalogo.Context;
+using Projeto_ApiCatalogo.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Projeto_ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+
+            try
+            {
+                var products = _context.Products.AsNoTracking().ToList();
+
+                if(products == null)
+                {                
+                    //return BadRequest("Valor nulo ou não encontrado");
+                }
+                return Ok(products);
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "[ERRO INTERNO]");
+            }
+
+
+        }
+
+        [HttpGet("{id:int}", Name ="GetProduct")]
+        public ActionResult<Product> Get(int id) 
+        {
+            var product = _context.Products.AsNoTracking().FirstOrDefault(e => e.ProductId == id);
+            if(product is null)
+            {
+                return NotFound("Registro não encontrado");
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Product product)
+        {
+            if(product is null)
+            {
+                return BadRequest();
+            }
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("GetProduct",new { id =product.ProductId }, product);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(product);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var product = _context.Products.FirstOrDefault(e => e.CategoryId == id);
+
+            if (product is null)
+            {
+                return NotFound() ;
+            }
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
+
+    }
+}
